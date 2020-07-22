8f6cd36b8f40b1ee4d9dd3cb06ed8404bbb9667e
wisdomosara <wisdomosara@gmail.com>
2020-07-22 12:55:37 +0100

    added

---
+++ johndoe.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    document.querySelector(".preload").classList.add("hide")
+    document.querySelector(".mainn").style.display = "block"
+    console.log("hidee")
+let hand = document.querySelector(".hand")
+let img = document.querySelector(".hand img")
+let clicked = false;
+
+    hand.addEventListener("click", function() {
+    clicked = !clicked
+if(clicked == true) {
+    img.src = "./images/icons8-delete-30.png"
+    console.log("open")
+}else {
+   img.src = "./images/hamburger.png"
+   }
+ })
+
+
+new WOW().init();
+let navLinks = document.querySelectorAll(".nav-item")
+
+navLinks.forEach(function(link) {
+    link.addEventListener("click", function() {
+        navLinks.forEach(function(links) {
+            links.classList.remove("active");
+        })
+        link.classList.add("active")
+    })
+})
+
+}
