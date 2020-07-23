b3bd961ce875c829c2478a1970689faba806df98
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-07-23 11:40:13 +0100

    Update johndoe.js

--- scripts/johndoe.js
+++ scripts/johndoe.js
@@ -2,19 +2,7 @@ window.onload = function() {
     document.querySelector(".preload").classList.add("hide")
     document.querySelector(".mainn").style.display = "block"
     console.log("hidee")
-let hand = document.querySelector(".hand")
-let img = document.querySelector(".hand img")
-let clicked = false;
 
-    hand.addEventListener("click", function() {
-    clicked = !clicked
-if(clicked == true) {
-    img.src = ".../assets/images/icons8-delete-30.png"
-    console.log("open")
-}else {
-   img.src = ".../assets/images/hamburger.png"
-   }
- })
 
 
 new WOW().init();
