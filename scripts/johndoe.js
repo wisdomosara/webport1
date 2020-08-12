fb2557d6c13d8853e5e0dc4eba8c13f068a27f0f
wisdomosara <wisdomosara@gmail.com>
2020-08-12 12:40:29 +0100

    added

--- scripts/johndoe.js
+++ scripts/johndoe.js
@@ -5,38 +5,38 @@ window.onload = function() {
     document.querySelector(".mainn").style.display = "block"
     console.log("hidee")
     let hand = document.querySelector(".hand")
-let img = document.querySelector(".hand img")
-let clicked = false;
+    let img = document.querySelector(".hand img")
+    let clicked = false;
 
     hand.addEventListener("click", function() {
-    clicked = !clicked
-if(clicked) {
-    img.src = "./assets/images/icons8-delete-30.png"
-    console.log("open")
-}else {
-   img.src = "./assets/images/hamburger.png"
-   }
- })
-    new WOW().init();
-let navLinks = document.querySelectorAll(".nav-item")
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
     clicked = !clicked
     if(clicked) {
         img.src = "./assets/images/icons8-delete-30.png"
         console.log("open")
     }else {
-       img.src = "./assets/images/hamburger.png"
-       }
-});
-navLinks.forEach(function(link) {
-    link.addEventListener("click", function() {
-        navLinks.forEach(function(links) {
-            links.classList.remove("active");
-        })
-        link.classList.add("active");
-        
+    img.src = "./assets/images/hamburger.png"
+    }
     })
-})
+    new WOW().init();
+    let navLinks = document.querySelectorAll(".nav-item")
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+        clicked = !clicked
+        if(clicked) {
+            img.src = "./assets/images/icons8-delete-30.png"
+            console.log("open")
+        }else {
+        img.src = "./assets/images/hamburger.png"
+        }
+    });
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function() {
+            navLinks.forEach(function(links) {
+                links.classList.remove("active");
+            })
+            link.classList.add("active");
+            
+        })
+    })
 
 }
