b25abfa062be1946aca6d5c270fb7fd35321d272
wisdomosara <50260832+wisdomosara@users.noreply.github.com>
2020-08-12 15:19:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.post('/formProcess', function (req, res) {
     var smtpTransport = nodemailer.createTransport("SMTP",{
        service: "Gmail", 
        auth: {
-       user: "email@gmail.com",
-       pass: "gmailPassword"
+       user: "wisdomosara@gmail.com",
+       pass: "Ikhuoria1996"
        }});
 
    smtpTransport.sendMail({  //email options
    from: "Sender Name <email@gmail.com>",
-   to: "Receiver Name <receiver@email.com>", // receiver
+   to: "Receiver Name <wisdomosara@email.com>", // receiver
    subject: "Emailing with nodemailer", // subject
    html: "here your data goes" // body (var data which we've declared)
     }, function(error, response){  //callback
