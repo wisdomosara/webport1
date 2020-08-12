d0849293cc6d55027d35746c3a86c557277ead3b
wisdomosara <wisdomosara@gmail.com>
2020-08-12 17:35:34 +0100

    added

---
+++ mail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = {
+        auth: {
+            api_key: '',
+            domain: ''
+        }
+};
+
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (name, email, subject, text, cb) => {
+    const mailOptions = {
+        sender: name,
+        from: email,
+        to: 'recipient@email.com',
+        subject: subject,
+        text: text
+    };
+
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            cb(err, null);
+        } else {
+            cb(null, data);
+        }
+    });
+
+// Exporting the sendmail
+module.exports = sendMail;
+}
--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-var express = require('express'),
-nodemailer = require("nodemailer");
+const express = require('express');
+const sendMail = require('./mail');
+const log = console.log;
+const app = express();
+const path = require('path');
+const router = express.Router();
+const PORT = 8080;
 
-app = express.createServer();
+// Configuring our data parsing
+app.use(express.urlencoded({
+    extend: false
+}));
+app.use(express.json());
 
-app.use(express.bodyParser());
+app.post('/email', (req, res) => {
+    // res.sendFile(path.join(__dirname + '/contact-us.html'));
+    //TODO
+    //send email here
+    const { name, subject, email, text } = req.body;
+    console.log('Data: ', req.body);
 
-app.post('/formProcess', function (req, res) {
-    var data=req.body;
+    sendMail(name, email, subject, text, function(err, data) {
+        if (err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            res.status({ message: 'Email sent!!!' });
+        }
+    });
+    // res.json({ message: 'Message received!!!' })
+});
 
-    var smtpTransport = nodemailer.createTransport("SMTP",{
-       service: "Gmail", 
-       auth: {
-       user: "wisdomosara@gmail.com",
-       pass: "Ikhuoria1996"
-       }});
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    //__dirname : It will resolve to your project folder.
+});
 
-   smtpTransport.sendMail({  //email options
-   from: "Sender Name <wisdomosara@gmail.com>",
-   to: "Receiver Name <wisdomosara@email.com>", // receiver
-   subject: "Emailing with nodemailer", // subject
-   html: "here your data goes" // body (var data which we've declared)
-    }, function(error, response){  //callback
-         if(error){
-           console.log(error);
-        }else{
-           console.log("Message sent: " + res.message);
-       }
-
-   smtpTransport.close(); 
-    }); });
+app.listen(PORT, () => log('Server is starting on PORT,', 8080));
