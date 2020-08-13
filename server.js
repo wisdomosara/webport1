4a485bfef435653005f54307365f8bc100ce050a
wisdomosara <wisdomosara@gmail.com>
2020-08-13 10:41:47 +0100

    added

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
-const express = require('express');
-const sendMail = require('./mail');
-const log = console.log;
-const app = express();
-const path = require('path');
-const router = express.Router();
-const PORT = 8080;
+// const express = require('express');
 
-// Configuring our data parsing
-app.use(express.urlencoded({
-    extend: false
-}));
-app.use(express.json());
+// const log = console.log;
+// const app = express();
+// const PORT = 3000;
 
-app.post('/email', (req, res) => {
-    // res.sendFile(path.join(__dirname + '/contact-us.html'));
-    //TODO
-    //send email here
-    const { name, subject, email, text } = req.body;
-    console.log('Data: ', req.body);
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+   
+// });
 
-    sendMail(name, email, subject, text, function(err, data) {
-        if (err) {
-            res.status(500).json({ message: 'Internal Error' });
-        } else {
-            res.status({ message: 'Email sent!!!' });
-        }
-    });
-    // res.json({ message: 'Message received!!!' })
-});
-
-router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-    //__dirname : It will resolve to your project folder.
-});
-
-app.listen(PORT, () => log('Server is starting on PORT,', 8080));
+// app.listen(PORT, () => log('Server is starting on PORT,', 8080));
