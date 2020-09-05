e233879eaa3c56d8d08aa4f73787e5db2c2fa743
wisdomosara <wisdomosara@gmail.com>
2020-09-05 21:37:37 +0100

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser  = require("body-parser")
 const log = console.log;
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.static("views"))
 app.use(bodyParser.urlencoded({extended: true}))
